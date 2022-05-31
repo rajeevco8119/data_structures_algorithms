e52b59bb9038222807fb39e9e040b9323d369f49
rajeevco8119 <rajeev.co8119@gmail.com>
2022-05-31 10:15:29 +0530

    Update hackerrank.py

--- hackerrank.py
+++ hackerrank.py
@@ -12,3 +12,27 @@ def timeConversion(s):
         hour += 12
         new_time = str(hour)+time[2:]
         return new_time
+
+#Staircase
+def staircase(n):
+    # Write your code here
+    for i in range(1,n+1):
+        a = n-i
+        h = i
+        while a > 0:
+            print(" ",end="")
+            a -= 1
+        while h > 0:
+            print('#',end="")
+            h -= 1
+        print('\r')
+
+# Staircase efficient
+def staircase2(n):
+    for i in range(0,n):
+        for _ in range(0, n):
+            if i + _ >= n-1:
+                print('#',end="")
+            else:
+                print(' ',end="")
+        print('\r')
