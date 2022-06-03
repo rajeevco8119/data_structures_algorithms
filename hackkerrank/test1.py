d9f5d4cf2deab70785378e64be41c7f98cb6a472
rajeevco8119 <rajeev.co8119@gmail.com>
2022-06-03 09:30:20 +0530

    Update test1.py

--- hackkerrank/test1.py
+++ hackkerrank/test1.py
@@ -27,3 +27,14 @@ def matchingStrings(strings, queries):
                 count += 1
         counts.append(count)
     return counts
+
+# Maximum sum using k consecutive elements using sliding window
+def maxSum(arr, k):
+    sum, max_sum = 0, 0
+    for i in range(len(arr)-k+1):
+        sum = 0
+        for j in range(i, i + k):
+            sum += arr[j]
+        if sum > max_sum:
+            max_sum = sum
+    print(max_sum)
